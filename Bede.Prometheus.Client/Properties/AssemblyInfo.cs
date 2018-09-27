c5ac5a5e414e0c59bfb4d76710f1cd12048845e8
Marian Stanev <marian.stanev@bedegaming.com>
2018-09-27 14:39:37 +0300

    add nuspec for Bede.Prometheus.Client and organize AssemblyInfo (#2)

--- Bede.Prometheus.Client/Properties/AssemblyInfo.cs
+++ Bede.Prometheus.Client/Properties/AssemblyInfo.cs
@@ -6,12 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Bede.Prometheus.Client")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bede.Prometheus.Client")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
--- Bede.Prometheus.Client_Tester.NetFramework/Properties/AssemblyInfo.cs
+++ Bede.Prometheus.Client_Tester.NetFramework/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Bede.Prometheus.Client_Tester.NetFramework")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bede.Prometheus.Client_Tester.NetFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
