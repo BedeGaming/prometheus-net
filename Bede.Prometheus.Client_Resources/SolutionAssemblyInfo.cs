40ab152d639b3291c71482a83f5b3a47131877a9
bede-marian-stanev <marian.stanev@bedegaming.com>
2018-09-27 12:23:37 +0300

    add support for 4.6.1 as Bede.Prometheus.Client

---
+++ Bede.Prometheus.Client_Resources/SolutionAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// This is the real version number, used in NuGet packages and for display purposes.
+[assembly: AssemblyFileVersion("1.0.0")]
+
+// Only use major version here, with others kept at zero, for correct assembly binding logic.
+[assembly: AssemblyVersion("1.0.0")]
+
+[assembly: InternalsVisibleTo("Bede.Prometheus.Client_Tests.NetFramework")]
+[assembly: InternalsVisibleTo("Bede.Prometheus.Client_Tests.NetCore")]
+[assembly: InternalsVisibleTo("Bede.Prometheus.Client_Benchmark.NetFramework")]
---
+++ Bede.Prometheus.Client_Tests.NetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bede.Prometheus.Client_Tests.NetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bede.Prometheus.Client_Tests.NetFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("870d6190-625c-4b01-b0c3-65c256e05aa6")]
